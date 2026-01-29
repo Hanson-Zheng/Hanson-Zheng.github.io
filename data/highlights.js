e301c1b7a64d4f72b3b7d93d7a6a6df30293f441
Hanson-Zheng <93787005+Hanson-Zheng@users.noreply.github.com>
2026-01-29 22:38:35 +0800

    Update highlights.js

--- data/highlights.js
+++ data/highlights.js
@@ -2,7 +2,7 @@ const highlightsData = [
     {
         "date": "2025-12-24",
         "title": "A milestone: MOTE on the cover of Nature Electronics.",
-        "image": "images/NE_COVER.jpeg",
+        "image": "images/NatureElectron.png",
         "description": "Our recent publication was featured on the cover of Nature Electronics (Volume 8 Issue 12, December 2025), highlighting our latest advances in tetherless neural recording."
     },
     {
