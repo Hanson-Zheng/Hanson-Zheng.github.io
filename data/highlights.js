3826f598adca1197f406a35dccbd7184a0bb0030
Hanson-Zheng <consummatezym@vip.qq.com>
2026-01-29 22:43:06 +0800

    Changed the min height of highlight cards to accommodate larger images.

--- data/highlights.js
+++ data/highlights.js
@@ -1,14 +1,14 @@
 const highlightsData = [
     {
-        "date": "2025-12-24",
-        "title": "A milestone: MOTE on the cover of Nature Electronics.",
-        "image": "images/NatureElectron.png",
-        "description": "Our recent publication was featured on the cover of Nature Electronics (Volume 8 Issue 12, December 2025), highlighting our latest advances in tetherless neural recording."
-    },
-    {
         "date": "2026-01-16",
         "title": "Paper Accepted in ICASSP 2026",
         "image": "images/ICASSP2026.jpg",
         "description": "We propose a closed-loop, data-driven method for robust detection and decoding of pulse-position modulation (PPM) signals over an optical link in implanted devices."
+    },
+    {
+        "date": "2025-12-24",
+        "title": "A milestone: MOTE on the cover of Nature Electronics.",
+        "image": "images/NatureElectron.png",
+        "description": "Our recent publication was featured on the cover of Nature Electronics (Volume 8 Issue 12, December 2025), highlighting our latest advances in tetherless neural recording."
     }
 ];
